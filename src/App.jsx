efa14a93d73853c314ef7e092aa58353abcf2d3a
Sherly Oktavia <sherlyoktavia.willisa@gmail.com>
2025-10-02 21:40:31 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -82,42 +82,54 @@ function App() {
       <div className="project pt-30 z-20">
         <p className="text-2xl md:text-5xl font-bold text-center pb-5 md:pb-10" data-aos="fade-in" id="project">My Project</p>
       <div>
-        <div className="border rounded-2xl border-zinc-800/70 items-center mx-7 mb-2 p-2 lg:mx-30 md:mb-7 md:p-4 flex gap-5" data-aos="fade-right">
-          <div className="flex flex-col">
+        <div className="border rounded-2xl border-zinc-800/70 items-center justify-center mx-7 mb-2 p-2 lg:mx-30 md:mb-7 md:p-4 flex gap-5" data-aos="fade-right">
+          <div className="flex flex-col ">
             <p className="text-xs sm:text-base font-semibold md:font-bold md:text-2xl">ReBio</p>
             <hr className=""/>
-            <p className="text-[6px]  sm:text-[10px] md:text-base text-justify mb-1 md:mb-2">A project to reducing organic waste, particularly household waste.</p>
-            <p className="text-[6px] sm:text-[10px] md:text-[14px] text-justify mb-1 md:mb-4 text-black/60">This is a group project that created for participating in the DOW: Heroes of Innovation competition which successfully bring us to 6th place in the competition. ReBio mobile application built with Flutter and Firebase, residents of the cluster will be able to do monitoring eco-enzyme condition based on sensor analysis, collaborate to ensure the eco-enzyme remains in healthy state, and interact each other within the system. </p>
+            <p className="text-[6px]  sm:text-[10px] md:text-base text-justify mb-1 md:mb-2">A project to reducing organic waste, particularly household waste from residential areas.</p>
+            <details>
+              <summary className="text-[6px] sm:text-[10px] md:text-[14px] text-justify mb-1 md:mb-4 text-black/60">See detail description</summary>
+              <p className="text-[6px] sm:text-[10px] md:text-[14px] text-justify mb-1 md:mb-4 text-black/60">This is a group project that created for participating in the DOW: Heroes of Innovation competition which successfully bring us to 6th place in the competition. ReBio mobile application built with Flutter and Firebase, residents of the cluster will be able to do monitoring eco-enzyme condition based on sensor analysis, collaborate to ensure the eco-enzyme remains in healthy state, and interact each other within the system. </p>
+            </details>
             <a href="https://github.com/sherlyoktavia/ReBio" target="_blank" className="border rounded-xl p-1 px-2 font-light md:p-2 md:px-4 border-zinc-800/70 md:font-semibold bg-sky-100 text-center w-fit text-[6px] sm:text-[10px] md:text-base hover:bg-sky-700/60 hover:text-white">GitHub <i className="ri-github-fill"></i></a>
           </div>
           <img src={Rebio} alt="Rebio" className="h-20 md:h-50 lg:h-60 xl:h-75"/>
         </div>
-        <div className="border-1 rounded-2xl border-zinc-800/70 items-center mx-7 mb-2 p-2 lg:mx-30 md:mb-7 md:p-5 flex justify-between gap-5" data-aos="fade-left">
+        <div className="border-1 rounded-2xl border-zinc-800/70 items-center mx-7 mb-2 p-2 lg:mx-30 md:mb-7 md:p-5 flex justify-center gap-5" data-aos="fade-left">
           <div className="flex flex-col">
             <p className="text-xs sm:text-base font-semibold md:font-bold md:text-2xl">CareerSprint</p>
             <hr className=""/>
-            <p className="text-[6px] sm:text-[10px] md:text-base text-justify mb-1 md:mb-2">This project is purposed to address employee retention, which is a major challenge faced by many big companies, also inefficient and suboptimal communication in the workplace.</p>
-            <p className="text-[6px] sm:text-[10px] md:text-[14px] text-justify mb-1 md:mb-4 text-black/60">Progress management feature displays a progress bar that shows each team’s performance and allows team members to receive or give feedback on their performance that can be viewed directly by teammates. Training Roadmap feature, which utilizes AI to perform analysis based on performance results, feedback, and employee capabilities during doing tasks to provide training recommendations.</p>
+            <p className="text-[6px] sm:text-[10px] md:text-base text-justify mb-1 md:mb-2">Application to address employee retention and solve inefficient & suboptimal communication.</p>
+            <details>
+              <summary className="text-[6px] sm:text-[10px] md:text-[14px] text-justify mb-1 md:mb-4 text-black/60">See detail description</summary>
+              <p className="text-[6px] sm:text-[10px] md:text-[14px] text-justify mb-1 md:mb-4 text-black/60">Employee retention become a major challenge faced by many big companies, also inefficient and suboptimal communication in the workplace. Progress management feature displays a progress bar that shows each team’s performance and allows team members to receive or give feedback on their performance that can be viewed directly by teammates. Training Roadmap feature, which utilizes AI to perform analysis based on performance results, feedback, and employee capabilities during doing tasks to provide training recommendations.</p>
+            </details>
             <a href="https://drive.google.com/file/d/1wlk4aFj5jfGJCnPreHluwpoQwF5_tK3Z/view" target="_blank" className="border rounded-xl p-1 px-2 font-light md:p-2 md:px-4 border-zinc-800/70 md:font-semibold bg-sky-100 text-center w-fit text-[6px] sm:text-[10px] md:text-base hover:bg-sky-700/60 hover:text-white">Google Drive <i className="ri-drive-fill"></i></a>
           </div>
           <img src={CareerSprint} alt="CareerSprint" className="h-20 md:h-50 lg:h-60 xl:h-75"/>
         </div>
-        <div className="border-1 rounded-2xl border-zinc-800/70 items-center mx-7 mb-2 p-2 lg:mx-30 md:mb-7 md:p-5 flex justify-between gap-5" data-aos="fade-right">
+        <div className="border-1 rounded-2xl border-zinc-800/70 items-center mx-7 mb-2 p-2 lg:mx-30 md:mb-7 md:p-5 flex justify-center gap-5" data-aos="fade-right">
           <div className="flex flex-col">
             <p className="text-xs sm:text-base font-semibold md:font-bold md:text-2xl">Weather Forecast Website</p>
             <hr className=""/>
             <p className="text-[6px] sm:text-[10px] md:text-base text-justify mb-1 md:mb-2">A Website to predict weather conditions for the next 5 days also temperature and humidity for the next 5 hours based on the location.</p>
-            <p className="text-[6px] sm:text-[10px] md:text-[14px] text-justify mb-1 md:mb-4 text-black/60">Weather analysis is conducted using data sourced from Kaggle and leveraging OpenWeatherMap API to provide more accurate result based on location. A Machine Learning model is used as the basis to provide prediction, with this project employing the Random Forest model, which achieved the highest accuracy compared to other models.</p>
+            <details>
+              <summary className="text-[6px] sm:text-[10px] md:text-[14px] text-justify mb-1 md:mb-4 text-black/60">See detail description</summary>
+              <p className="text-[6px] sm:text-[10px] md:text-[14px] text-justify mb-1 md:mb-4 text-black/60">Weather analysis is conducted using data sourced from Kaggle and leveraging OpenWeatherMap API to provide more accurate result based on location. A Machine Learning model is used as the basis to provide prediction, with this project employing the Random Forest model, which achieved the highest accuracy compared to other models.</p>
+            </details>
             <a href="https://github.com/sherlyoktavia/Weather-Forecast-Website" target="_blank" className="border rounded-xl p-1 px-2 font-light md:p-2 md:px-4 border-zinc-800/70 md:font-semibold bg-sky-100 text-center w-fit text-[6px] sm:text-[10px] md:text-base hover:bg-sky-700/60 hover:text-white">GitHub <i className="ri-github-fill"></i></a>
           </div>
           <img src={Weather} alt="Weather Forecast" className="w-25 md:w-60 lg:w-80 xl:w-93"/>
         </div>
-        <div className="border-1 rounded-2xl border-zinc-800/70 items-center mx-7 mb-2 p-2 lg:mx-30 md:mb-7 md:p-5 flex justify-between gap-5" data-aos="fade-left">
+        <div className="border-1 rounded-2xl border-zinc-800/70 items-center mx-7 mb-2 p-2 lg:mx-30 md:mb-7 md:p-5 flex justify-center gap-5" data-aos="fade-left">
           <div className="flex flex-col">
             <p className="text-xs sm:text-base font-semibold md:font-bold md:text-2xl">Sobat</p>
             <hr className=""/>
-            <p className="text-[6px] sm:text-[10px] md:text-base text-justify mb-1 md:mb-2">Application that connects food producers and distributors in the form of government agencies, and beneficiary communities to ensure efficient and targeted food distribution.</p>
-            <p className="text-[6px] sm:text-[10px] md:text-[14px] text-justify mb-1 md:mb-4 text-black/60">This application also serves as an effort to reduce food waste, encourage optimal consumption of local food, and connect directly with small farmers/local food communities. There are two roles available when accessing the application: Authority (Government Agency) or Friend (General Public), each leading to a different application page based on their respective roles.</p>
+            <p className="text-[6px] sm:text-[10px] md:text-base text-justify mb-1 md:mb-2">Application to ensure efficient and targeted food distribution, reduce food waste, and encourage optimal consumption.</p>
+            <details>
+              <summary className="text-[6px] sm:text-[10px] md:text-[14px] text-justify mb-1 md:mb-4 text-black/60">See detail description</summary>
+              <p className="text-[6px] sm:text-[10px] md:text-[14px] text-justify mb-1 md:mb-4 text-black/60">This application  connects food producers and distributors in the form of government agencies, and beneficiary communities. It also serves as an effort to reduce food waste, encourage optimal consumption of local food, and connect directly with small farmers/local food communities. There are two roles available when accessing the application: Authority (Government Agency) or Friend (General Public), each leading to a different application page based on their respective roles.</p>
+            </details>
             <a href="https://www.figma.com/proto/oMymoqTCgfoY4i5JwSbnIH/Techfest?node-id=139-7060&t=dhTrdTRnrf7BpuSF-1&starting-point-node-id=139%3A7050" target="_blank" className="border rounded-xl p-1 px-2 font-light md:p-2 md:px-4 border-zinc-800/70 md:font-semibold bg-sky-100 text-center w-fit text-[6px] sm:text-[10px] md:text-base hover:bg-sky-700/60 hover:text-white">Figma <i className="ri-figma-fill"></i></a>
           </div>
           <img src={Sobat} alt="Sobat" className="w-25 md:w-60 lg:w-75 xl:w-93"/>
